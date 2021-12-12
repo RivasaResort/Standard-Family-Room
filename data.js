f5492ce23018fc94fe2ff7e4ab460ac6ef2ef5a0
Sanju Kamat <sanjukamat123@gmail.com>
2021-12-12 19:40:26 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,270 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.45177382541236,
+        "pitch": 0.051348519419093464,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4879603622328954,
+          "pitch": 0.13610103762844972,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": 0.607045706634123,
+          "pitch": 0.09733731393903966,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4150648640373067,
+          "pitch": -0.22485489662399516,
+          "title": "STANDARD FAMILY ROOM",
+          "text": "<div>Amenities&nbsp;</div><div><br></div><div>- AC</div><div>- TV</div><div>- Sanitized Rooms&nbsp;</div><div>- Daily Housekeeping&nbsp;</div><div>- Free Wifi</div><div>- Swimming pool</div><div>- In house Restaurant &amp; Bar</div><div>- Parking Facility&nbsp;</div><div>- 5 Min walk to the Beach</div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.0305423217541136,
+        "pitch": 0.4591256018079246,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.023916386347393,
+          "pitch": 0.10869130136858018,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.883183039992847,
+        "pitch": 0.346378947930539,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5348666974854286,
+          "pitch": 0.0966028990386043,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": -2.638781122513148,
+          "pitch": 0.04862369416604295,
+          "rotation": 0,
+          "target": "3-4"
+        },
+        {
+          "yaw": 1.5353060110485792,
+          "pitch": 0.0834521593530333,
+          "rotation": 0,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.018431614817688,
+        "pitch": 0.03942629821619015,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14421304802422696,
+          "pitch": 0.10881581526295214,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.6639757999917375,
+        "pitch": 0.28773734820564556,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6669985272803238,
+          "pitch": 0.08857566901041736,
+          "rotation": 0,
+          "target": "5-6"
+        },
+        {
+          "yaw": -0.8846144712242925,
+          "pitch": 0.045750608993838426,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.3555615192676846,
+        "pitch": 0.7181388936173203,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3937399021242332,
+          "pitch": 0.045594790417828435,
+          "rotation": 0,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
